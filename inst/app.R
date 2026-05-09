2d5e3dd9ef1c191bcb3564e32d8c9ebd5f0452e9
mhem <mhem@mailbox.org>
2026-05-09 11:59:42 +0200

    change app.R, required for testing

--- inst/app.R
+++ inst/app.R
@@ -1,15 +1,43 @@
 ##----------------------------------------------------------------------------##
-## set .libPaths() to load packages from inside Cerebro standalone version
+## load packages
 ##----------------------------------------------------------------------------##
-if ( grepl(tolower(Sys.info()["sysname"]), pattern = "^win") )
-{
-  .libPaths(paste0(getwd(), "/R-Portable-Win/library"))
-} else if ( grepl(tolower(Sys.info()["sysname"]), pattern = "darwin") )
-{
-  .libPaths(paste0(getwd(), "/R-Portable-Mac/library"))
-}
+library(shiny)
+library(shinydashboard)
+library(shinyWidgets)
+library(shinyjs)
+library(DT)
+library(plotly)
+library(dplyr)
 
 ##----------------------------------------------------------------------------##
-## launch Cerebro interface
+## set options
 ##----------------------------------------------------------------------------##
-cerebroAppLite::launchCerebro()
+custom_welcome_message <- "Welcome to Cerebro! This is a custom welcome message. You can change it in the app options."
+Cerebro.options <<- list(
+  "mode" = "closed",
+  "crb_file_to_load" = "extdata/v1.4/example.crb",
+  "expression_matrix_mode" = "h5",
+  "expression_matrix_h5" = "extdata/v1.4/example.h5",
+  "cerebro_root" = ".",
+  "welcome_message" = custom_welcome_message,
+  "overview_default_point_size" = 1,
+  "gene_expression_default_point_size" = 2,
+  "overview_default_point_opacity" = 0.3,
+  "gene_expression_default_point_opacity" = 0.5,
+  "overview_default_percentage_cells_to_show" = 100,
+  "gene_expression_default_percentage_cells_to_show" = 20,
+  "projections_show_hover_info" = FALSE
+)
+
+options(shiny.maxRequestSize = 800 * 1024^2)
+
+##----------------------------------------------------------------------------##
+## load server and UI functions
+##----------------------------------------------------------------------------##
+source("shiny/v1.4/shiny_UI.R", local = TRUE)
+source("shiny/v1.4/shiny_server.R", local = TRUE)
+
+##----------------------------------------------------------------------------##
+## launch app
+##----------------------------------------------------------------------------##
+shiny::shinyApp(ui = ui, server = server)
