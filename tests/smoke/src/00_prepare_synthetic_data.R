feb7a426e859f604039a95c7c5dd7f9ab24a53b7
xuesong wang <wangxuesong29@gmail.com>
2026-05-27 02:18:02 +0200

    chore: stop tracking smoke tests and benchmark vignette

--- tests/smoke/src/00_prepare_synthetic_data.R
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env Rscript
-
-rm(list = ls())
-
-source("src/smoke_fixture_utils.R")
-ensure_smoke_fixtures(
-  data_dir = "data",
-  force = identical(Sys.getenv("SMOKE_SYNTH_FORCE", unset = "0"), "1")
-)
